d98206e322ae50823594e965522a66363211031e
Caleb Hemphill <calebhemp@gmail.com>
2023-10-30 21:23:17 -0400

    create calculateSales function

--- js/app.js
+++ js/app.js
@@ -1,28 +1,49 @@
 'use strict';
 
+let hoursOpen = ['6am', '7am', '8am'];
+
+// calculate random customers for each hour
 function randomCustomers (min, max) {
   let customers = [];
-  for (let i = 0; i < 14; i++) {
-    customers.push(Math.floor(Math.random() * (max - min + 1) + min));
+  for (let i = 0; i < 14; i++) { // calculate each hour customers
+    let randomCustomer = (Math.floor(Math.random() * (max - min + 1) + min));
+    customers.push(randomCustomer);
   }
   return customers;
 }
 
-function calculateSales (estCustomers, avgSale) {
-
+// calculate sales for each hour and total for day
+function calculateSales (estCustomers, avgSale) { 
+  let sales = [];
+  let totalSales = 0;
+  for (let i = 0; i < 14; i++) { // calculate each hour sales
+    sales.push(Math.floor(estCustomers[i] * avgSale));
+  }
+  for (let i = 0; i < sales.length; i++) { // calculate total day sales
+    totalSales += sales[i];
+  }
+  sales.push(totalSales); // stores total day sales at end of the array
+  return sales;
 }
+
+/*
+
+*/
 
 let seattle = {
   location: 'Seattle',
   minHourlyCustomers: 23,
   maxHourlyCustomers: 65,
   avgSalePerCustomer: 6.3,
-  estCustomersByHour: [],
-  estSalesByHour: [],
+  estCustomers: [],
+  estSales: [],
   generateEstCustomers: function () {
-    this.estCustomersByHour = randomCustomers(this.minHourlyCustomers, this.maxHourlyCustomers);
+    this.estCustomers = randomCustomers(this.minHourlyCustomers, this.maxHourlyCustomers);
   },
   generateEstSales: function () {
-    this.estSalesByHour = calculateSales(this.estCustomersByHour, this.avgSalePerCustomer);
+    this.estSales = calculateSales(this.estCustomers, this.avgSalePerCustomer);
   }
 };
+
+seattle.generateEstCustomers();
+seattle.generateEstSales();
